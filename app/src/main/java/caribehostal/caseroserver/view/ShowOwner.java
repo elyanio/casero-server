ea4d41905d1fb048047dd851f662e753652fbef1
yanier <yanier.alfonso@ssp.jovenclub.cu>
2017-08-22 13:00:01 -0400

    Crear el cardView para mostrar los itmes de los Owner

---
+++ app/src/main/java/caribehostal/caseroserver/view/ShowOwner.java
@@ -0,0 +1,16 @@
+package caribehostal.caseroserver.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import caribehostal.caseroserver.R;
+
+public class ShowOwner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_owner);
+
+    }
+}

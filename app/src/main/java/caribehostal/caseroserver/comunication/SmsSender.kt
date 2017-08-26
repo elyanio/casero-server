1a08a431b66bf90316a772546a5f74394af91524
Rainer Martínez Fraga <rainermf@polymitasoft.com>
2017-08-26 17:36:32 -0400

    Fixing code smells.

--- app/src/main/java/caribehostal/caseroserver/comunication/SmsSender.kt
+++ app/src/main/java/caribehostal/caseroserver/comunication/SmsSender.kt
@@ -1,23 +1,21 @@
-package caribehostal.appcasero.comunication
+package caribehostal.caseroserver.comunication
 
 import android.telephony.SmsManager
 
 /**
- * Created by asio on 8/17/2017.
- */
+* @author asio
+*/
 class SmsSender {
 
-    fun enviar_mensaje(numero: String, mensaje: String) {
-        val sms = SmsManager.getDefault()
-        sms.sendTextMessage(numero, null, mensaje, null, null)
+    val smsManager = SmsManager.getDefault()
+
+    fun enviarMensaje(numero: String, mensaje: String) {
+        smsManager.sendTextMessage(numero, null, mensaje, null, null)
     }
 
-    fun enviar_mensaje(numbers: List<String>, mensaje: String) {
-        val sms = SmsManager.getDefault()
-        if (numbers.size != 0) {
-            for (number in numbers) {
-                sms.sendTextMessage(number, null, mensaje, null, null)
-            }
+    fun enviarMensaje(numbers: List<String>, mensaje: String) {
+        for (number in numbers) {
+            enviarMensaje(number, mensaje)
         }
     }
 }

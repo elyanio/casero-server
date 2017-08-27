7e282015760b08cffb1379b5f3e04600df46b199
asio <asio@polymitasoft.com>
2017-08-27 17:03:16 -0600

    Create ActioDetailView.

---
+++ app/src/main/java/caribehostal/caseroserver/view/action/ActionDetail.java
@@ -0,0 +1,15 @@
+package caribehostal.caseroserver.view.action;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import caribehostal.caseroserver.R;
+
+public class ActionDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_detail);
+    }
+}

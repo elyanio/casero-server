267aacbd7d553b3945c281806dfc0fec82da2073
Rainer Martínez Fraga <rainermf@polymitasoft.com>
2017-08-30 12:22:23 -0400

    Añadir contraseña a vista de Reportes.

--- app/src/main/java/caribehostal/caseroserver/view/report/ReportActivity.kt
+++ app/src/main/java/caribehostal/caseroserver/view/report/ReportActivity.kt
@@ -2,31 +2,29 @@ package caribehostal.caseroserver.view.report
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import butterknife.OnClick
+import android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD
 import caribehostal.caseroserver.R
 import caribehostal.caseroserver.util.toDate
 import caribehostal.caseroserver.util.toLocalDate
 import com.squareup.timessquare.CalendarPickerView.SelectionMode.RANGE
 import kotlinx.android.synthetic.main.activity_report.*
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.onClick
-import org.jetbrains.anko.toast
-import org.jetbrains.anko.uiThread
+import org.jetbrains.anko.*
 import org.threeten.bp.LocalDate
 
 class ReportActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_report)
-
-        val today = LocalDate.now()
-        val start = today.minusYears(2)
-        val end = today.plusYears(2)
-        calendar_view.init(start.toDate(), end.toDate())
-                .withSelectedDate(today.toDate())
-                .inMode(RANGE)
-        create_report_button.onClick { createReport() }
+        afterAuthentication {
+            setContentView(R.layout.activity_report)
+            val today = LocalDate.now()
+            val start = today.minusYears(2)
+            val end = today.plusYears(2)
+            calendar_view.init(start.toDate(), end.toDate())
+                    .withSelectedDate(today.toDate())
+                    .inMode(RANGE)
+            create_report_button.onClick { createReport() }
+        }
     }
 
     fun createReport() {
@@ -37,8 +35,29 @@ class ReportActivity : AppCompatActivity() {
         doAsync {
             val file = ActionViewSpec.createPdf(start, end)
             uiThread {
-                ActionViewSpec.displayPdf(this@ReportActivity, file)
+                ActionViewSpec.displayPdf(ctx, file)
             }
         }
+    }
+
+    inline fun afterAuthentication(crossinline action: () -> Unit) {
+        val dialog = AlertDialogBuilder(ctx)
+        with(dialog) {
+            title = "Password"
+            customView {
+                verticalLayout {
+                    val passwordInput = editText {
+                        inputType = TYPE_TEXT_VARIATION_PASSWORD
+                    }
+                    button(text = "Aceptar").onClick {
+                        if(passwordInput.text.toString() == "02113229") {
+                            dialog.dismiss()
+                            action.invoke()
+                        }
+                    }
+                }
+            }
+        }
+        dialog.show()
     }
 }

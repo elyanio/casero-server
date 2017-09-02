f337c8b57e8ef72615cff3f32dcf7d3d5ef9dcb7
asio <asio@polymitasoft.com>
2017-09-02 01:09:26 -0600

    Arreglando la vista principal

--- app/src/main/java/caribehostal/caseroserver/MainActivity.java
+++ app/src/main/java/caribehostal/caseroserver/MainActivity.java
@@ -24,24 +24,24 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.button_owner)
+    @OnClick(R.id.card_view_owner)
     void showOwnerView() {
         startActivity(new Intent(this, OwnerShow.class));
     }
 
-    @OnClick(R.id.button_register)
+    @OnClick(R.id.card_view_register)
     void showRegisterView() {
         Bundle bundle = new Bundle();
         bundle.putInt(OwnerRegister.INTENT_ACTION, OwnerRegister.ACTION_INSERT);
         startActivity(new Intent(this, OwnerRegister.class).putExtras(bundle));
     }
 
-    @OnClick(R.id.button_action)
+    @OnClick(R.id.card_view_action)
     void showActionView() {
         startActivity(new Intent(MainActivity.this, ActionShow.class));
     }
 
-    @OnClick(R.id.button_report)
+    @OnClick(R.id.card_view_report)
     void showReportView() {
         startActivity(new Intent(this, ReportActivity.class));
     }

6f7e1dbce11fdb1cbe42e268f59d313698a5cb09
asio <asio@polymitasoft.com>
2017-09-26 12:57:42 -0600

    create insertOwner on SmsReceiverOwnerController.

--- app/src/main/java/caribehostal/caseroserver/controller/SmsReceiverOwnerController.java
+++ app/src/main/java/caribehostal/caseroserver/controller/SmsReceiverOwnerController.java
@@ -1,5 +1,8 @@
 package caribehostal.caseroserver.controller;
 
+import caribehostal.caseroserver.dataaccess.DaoOwner;
+import caribehostal.caseroserver.datamodel.Owner;
+
 /**
  * Created by asio on 9/26/2017.
  */
@@ -7,11 +10,27 @@ package caribehostal.caseroserver.controller;
 public class SmsReceiverOwnerController {
 
     private String[] values;
+    private static int UNREGISTERED = 0;
 
     public SmsReceiverOwnerController(String cell, String smsBody) {
         values = smsBody.split("#");
+        insertOwner();
     }
 
-
+    private void insertOwner() {
+        DaoOwner daoOwner = new DaoOwner();
+        if (daoOwner.getOwnerByCell(values[3]) == null) {
+            Owner owner = new Owner();
+            owner.setFullName(values[1]);
+            owner.setCarnetId(values[2]);
+            owner.setCell(values[3]);
+            owner.setUser(values[4]);
+            owner.setPassword(values[5]);
+            owner.setAddress(values[6]);
+            owner.setAddressDescription(values[7]);
+            owner.setRegister(UNREGISTERED);
+            daoOwner.upsertOwner(owner);
+        }
+    }
 
 }
